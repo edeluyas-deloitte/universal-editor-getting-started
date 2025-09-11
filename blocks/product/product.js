2e338c549d86ee9889cb17ef879b0a994918621e
Emmanuel Zion Deluyas <edeluyas@deloitte.com>
2025-09-11 18:40:31 +0800

    Updated definitions

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,6 +1,7 @@
 import { getMetadata } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
+  console.log(block);
   const aemauthorurl = getMetadata('authorurl') || '';
   const aempublishurl = getMetadata('publishurl') || '';
   console.log(aemauthorurl, aempublishurl);
