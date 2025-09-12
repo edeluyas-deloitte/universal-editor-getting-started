9fac5b035e55c3a948dc8f699e1052965a634c64
Emmanuel Zion Deluyas <edeluyas@deloitte.com>
2025-09-12 12:09:34 +0800

    Updated product block

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,17 +1,21 @@
 export default async function decorate(block) {
   console.block(block);
-  const url = 'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/ez-eds/get-credit-card-products';
+  const url =
+    "https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/ez-eds/get-credit-card-products";
 
   const cfRequest = await fetch(url)
-  .then((response) => response.json())
-  .then((contentFragment) => {
-    let products = '';
-    if (contentFragment.data) {
-      products = contentFragment.data;
-    }
-    return products;
-  }).catch((error) => {
-    console.error('Error fetching data:', error);
-    return null;
-  });
+    .then((response) => response.json())
+    .then((contentFragment) => {
+      let products = "";
+      if (contentFragment.data) {
+        products = contentFragment.data;
+      }
+      return products;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      return null;
+    });
+
+  console.log("cfRequest", cfRequest);
 }
