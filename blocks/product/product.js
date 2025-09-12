d3cad49a932ab51eecd3a7d11f1ac2d3bcf0677f
Emmanuel Zion Deluyas <edeluyas@deloitte.com>
2025-09-12 12:12:28 +0800

    Fixd sonarlint issues

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,21 +1,20 @@
 export default async function decorate(block) {
   console.block(block);
-  const url =
-    "https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/ez-eds/get-credit-card-products";
+  const url = 'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/ez-eds/get-credit-card-products';
 
   const cfRequest = await fetch(url)
     .then((response) => response.json())
     .then((contentFragment) => {
-      let products = "";
+      let products = '';
       if (contentFragment.data) {
         products = contentFragment.data;
       }
       return products;
     })
     .catch((error) => {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
       return null;
     });
 
-  console.log("cfRequest", cfRequest);
+  console.log('cfRequest', cfRequest);
 }
