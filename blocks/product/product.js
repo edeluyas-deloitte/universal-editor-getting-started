88ab4b6995ee298bcd8f2c8097033211f6a0e399
Emmanuel Zion Deluyas <edeluyas@deloitte.com>
2025-09-15 15:57:52 +0800

    Updated css and product js

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,22 +1,55 @@
 export default async function decorate(block) {
-  console.log(block);
-  const url = 'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/ez-eds/get-credit-card-products';
+  const authorurl = 'https://author-p9606-e71941.adobeaemcloud.com'
+  const graphUrl = '/graphql/execute.json/ez-eds/get-product-by-path;path=';
+
+  const contentPath =
+    block
+      .querySelector(':scope div:nth-child(1) > p > a')
+      ?.getAttribute('href') || '';
+
+  const url = `${authorurl}${graphUrl}${contentPath}`;
 
   const options = { credentials: 'include' };
-
-  const cfRequest = await fetch(url, options)
+  const data = await fetch(url, options)
     .then((response) => response.json())
-    .then((contentFragment) => {
-      let products = '';
-      if (contentFragment.data) {
-        products = contentFragment.data.productList;
+    .then((cf) => {
+      let cfData = '';
+      if (cf.data) {
+        cfData = cf.data.productByPath.item;
       }
-      return products;
+      return cfData;
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
       return null;
     });
 
-  console.log('cfRequest', cfRequest);
+  const buttonP = block.querySelector(':scope p.button-container');
+  if (buttonP) {
+    buttonP.remove();
+  }
+
+  const buttonDiv = block.querySelector(':scope div:nth-child(2)> div');
+
+  buttonDiv.classList.add('product-button');
+
+  const lastChildDiv = block.querySelector(':scope div:nth-child(2)');
+
+  if (lastChildDiv) {
+    lastChildDiv.remove();
+  }
+
+  const contentDiv = block.querySelector(':scope div:nth-child(1)');
+  contentDiv.classList.add('product-content');
+
+  contentDiv.innerHTML = `
+    <picture class="product-image">
+      <img loading="lazy" src="${authorurl}${data.productImage._path}" alt="${data.productName}" />
+    </picture>
+    <div class="product-info">
+      <h3>${data.productName}</h3>
+      ${data.productDescription.html}
+    </div>
+    ${buttonDiv.outerHTML}
+  `;
 }
